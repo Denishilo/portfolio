264e4af041c0d2073f970405ea285f23e4b15e63
Denishilo <denisanishenko@gmail.com>
2023-07-22 15:36:17 +0300

    feat: add main section

--- src/layout/main/Main.tsx
+++ src/layout/main/Main.tsx
@@ -1,22 +1,42 @@
 import React from 'react';
 import styled from "styled-components";
 import photo from '../../images/photo.jpeg'
+import {FlexWrapper} from "components/flexWrapper/FlexWrapper";
 
 export const Main = () => {
     return (
-        <div>
-            <div>
-                <span>Hi there!</span>
-                <span>I'm Denis Anishchenko</span>
-                <h1>a Frontend Developer,</h1>
-                <p>experienced in React/Redux/TypeScript/JavaScript. I’d really love to have an opportunity to do
-                    different complicated and absorbing tasks, with an eye to improving my skills. At the exact moment,
-                    I’m expanding my knowledge in Next.js and Node.js. </p>
-            </div>
-            <Photo src={photo} alt="photo"/>
-        </div>
+        <StyledMain>
+            <FlexWrapper align={'center'} justify={'space-around'}>
+                <div>
+                    <span>Hi there!</span>
+                    <Name>I'm Denis Anishchenko</Name>
+                    <MainTitle>a Frontend Developer,</MainTitle>
+                    <Info>experienced in React/Redux/TypeScript/JavaScript. I’d really love to have an opportunity to do
+                        different complicated and absorbing tasks, with an eye to improving my skills. At the exact
+                        moment, I’m expanding my knowledge in Next.js and Node.js. </Info>
+                </div>
+                <Photo src={photo} alt="photo"/>
+            </FlexWrapper>
+
+        </StyledMain>
     );
 };
+
+const StyledMain = styled.div`
+  min-height: 100vh;
+  background-color: antiquewhite;
+`
+
+const MainTitle = styled.h1`
+`
+
+const Name = styled.h2`
+`
+
+const Info = styled.p`
+    max-width: 400px;
+`
+
 
 const Photo = styled.img`
   width: 350px;
